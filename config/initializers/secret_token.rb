d9c2e943bb7f89479e3e89664ce87b2f2ad0eeeb
csigi <trannguyenbaotrung@gmail.com>
2013-03-03 21:47:25 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sagwija::Application.config.secret_token = 'd46083c3245416ad84a093ed8366dd75e0394640ecd6156b8b3026b7fe5c7bc0ecb1e18d5eccb5d316a74dcb0afe565b0832e1552688af2146acc5742ce9b735'
